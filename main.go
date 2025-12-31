197a5984a2b68d1d73ed149de7f572dc5df11644
Kenjiro56 <hiroken506@gmail.com>
2025-12-31 15:48:35 +0900

    feat/multiport

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 
 func main() {
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			// port is closed or filtered.
-			continue
-		}
-		conn.Close()
-		fmt.Printf("%d open\n", i)
+		go func(j int) {
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
 	}
 }
